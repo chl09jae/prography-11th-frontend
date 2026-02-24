7e766c5288e534b2e157f838727f0994ab60d8ad
최영재 <chldudwo97@aromit.com>
2026-02-24 16:22:26 +0900

    feat: [환경세팅] 초기 환경 세팅 커밋
    React+Vite+TypeScript 설치

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    return (
+        <div className="flex justify-center items-center h-screen bg-blue-500">
+            <h1 className="text-4xl font-bold text-white underline">
+                Prography 11th front 과제 시작하기
+            </h1>
+        </div>
+    )
+}
+
+export default App
